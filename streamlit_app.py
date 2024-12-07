866362017a15f3d4f19826f255b410a4d6d6b568
Joe <joecn2704+github@gmail.com>
2024-12-07 11:55:16 +0400

    yea

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,12 +19,12 @@ st.write(
 
 st.header("How to use the App?")
 st.write(
-    "To use the App, simply input any text in the text box provided and click on the **Predict** button. The model will then predict the font used in the input."
+    "To use the App, simply input the character in the text box provided and upload a screenshot of the same character and click on the **Predict** button. The model will then predict the font used in the input."
 )
 
 st.header("About the Dataset")
 st.write(
-    "The dataset used to train the model is the **Font Dataset**. The dataset contains images of fonts and their corresponding labels."
+    "The dataset used to train the model is the **[Font Dataset](https://archive.ics.uci.edu/dataset/417/character+font+images)**. The dataset contains images from 153 character fonts."
 )
 
 st.header("About the Model")
@@ -41,17 +41,22 @@ st.write("- **Eman**\n" "- **Joe**\n" "- **Sreeni**\n" "- **Yusra**\n")
 st.divider()
 
 st.header("Inputs")
-col1, col2 = st.columns(2)
-with col1:
-    character = st.text_input("Enter Character", max_chars=1)
+with st.container():
+    col1, col2 = st.columns(2)
+    with col1:
+        character = st.text_input("Enter Character", max_chars=1)
 
-with col2:
-    with st.container():
-        font_image = st.file_uploader("Upload Image", type=["jpg", "png", "jpeg"])
-
-        if font_image is not None:
-            st.image(
-                font_image, caption=f"Character: {character}", use_container_width=True
+    with col2:
+        with st.container():
+            font_image = st.file_uploader(
+                "Upload Image", type=["jpg", "png", "jpeg"], accept_multiple_files=False
             )
 
-predict_button = st.button("Predict")
+            if font_image is not None:
+                st.image(
+                    font_image,
+                    caption=f"Character: {character}",
+                    use_container_width=True,
+                )
+
+    predict_button = st.button("Predict", use_container_width=True)
